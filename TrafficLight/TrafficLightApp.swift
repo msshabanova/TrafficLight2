6cf22a5d80c4b52e68e51cb2ff060ee0124faf7a
Мария Шабанова <mariasha312@gmail.com>
2024-04-17 18:22:00 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Мария Шабанова on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
